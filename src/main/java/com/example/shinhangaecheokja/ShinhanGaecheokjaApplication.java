092b95037aca598b50fb84544d718ae0459a4730
SungminJo <sungminjo@SungminJos-MacBook-Pro.local>
2026-07-23 17:35:33 +0900

    first commit

---
+++ src/main/java/com/example/shinhangaecheokja/ShinhanGaecheokjaApplication.java
@@ -0,0 +1,13 @@
+package com.example.shinhangaecheokja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinhanGaecheokjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShinhanGaecheokjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shinhangaecheokja/ShinhanGaecheokjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shinhangaecheokja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinhanGaecheokjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
