ef5150431bd6f47eff830c76ace49ea1512c65ac
kms7522 <75304079+kms7522@users.noreply.github.com>
2026-07-24 17:49:23 +0900

    feat: Vehicle 엔티티 및 CRUD 기능 추가 (#18)
    * feat: Vehicle 엔티티 추가
    
    * feat: Vehicle owner_id에 Member 참조 FK 제약 추가
    
    * feat: Vehicle CRUD 기능 추가
    
    * test: Vehicle CRUD 테스트 추가
    
    * docs: Vehicle 관련 클래스/메서드에 설명 주석 추가
    
    * refactor: Vehicle 도메인을 도메인 기준 패키지 구조로 이동, Member 도메인 직접 참조 제거

---
+++ src/main/java/com/example/shinhangaecheokja/vehicle/repository/VehicleRepository.java
@@ -0,0 +1,7 @@
+package com.example.shinhangaecheokja.vehicle.repository;
+
+import com.example.shinhangaecheokja.vehicle.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/** Vehicle 엔티티에 대한 JPA 저장소. */
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {}
