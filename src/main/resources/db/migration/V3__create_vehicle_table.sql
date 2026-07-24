ced2db826682f257f802e87edf67144d5ffa921f
kms <kms75229911@gmail.com>
2026-07-24 15:31:09 +0900

    feat: Vehicle owner_id에 Member 참조 FK 제약 추가

--- src/main/resources/db/migration/V3__create_vehicle_table.sql
+++ src/main/resources/db/migration/V3__create_vehicle_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE vehicle (
     owner_id BIGINT NOT NULL,
     type VARCHAR(20) NOT NULL,
     max_weight DOUBLE NOT NULL,
-    max_distance DOUBLE NOT NULL
+    max_distance DOUBLE NOT NULL,
+    CONSTRAINT fk_vehicle_owner FOREIGN KEY (owner_id) REFERENCES member (id)
 );
