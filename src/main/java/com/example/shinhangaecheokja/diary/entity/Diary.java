1c4069791b0227b04024fed532fda7063f15a906
user <user@gmail.com>
2026-07-24 10:51:18 +0900

    배고파요

--- src/main/java/com/example/shinhangaecheokja/diary/entity/Diary.java
+++ src/main/java/com/example/shinhangaecheokja/diary/entity/Diary.java
@@ -75,6 +75,7 @@ public class Diary {
         this.content = content;
         this.emotionId = emotionId;
     }
+    
     public void update5(Long date, String content, Integer emotionId) {
         this.date = date;
         this.content = content;
