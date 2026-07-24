cc4f2bce647ee7b1e7f2487654d8d7afb9b6bbb2
전민욱 <mwzzang00@gmail.com>
2026-07-24 12:21:40 +0900

    UserRepository커밋

--- src/main/java/com/example/shinhangaecheokja/repository/UserRepository.java
+++ src/main/java/com/example/shinhangaecheokja/repository/UserRepository.java
@@ -7,7 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
-
-    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);   //데이터베이스에서 이메일이 똑같은 회원을 찾아서 가져와
+                                                //optimal덕분에 없어도 에러가 안남
+    boolean existsByEmail(String email);    //데이터베이스에 이메일이 똑같은 외원이 존재하는지 확인
 }
+//회원가입할때 중복 이메일이 있는지 스캔(existByEmail)하고 로그인할떄 회원의 정보를 안전하게 꺼내오는(findByEmail) 역할
