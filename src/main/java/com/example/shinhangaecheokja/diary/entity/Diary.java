38ac3900681188e711777b0e81fb1a10c1caf8ce
user <user@gmail.com>
2026-07-24 09:31:55 +0900

    해진의 두번째 커밋

--- src/main/java/com/example/shinhangaecheokja/diary/entity/Diary.java
+++ src/main/java/com/example/shinhangaecheokja/diary/entity/Diary.java
@@ -64,4 +64,11 @@ public class Diary {
         this.content = content;
         this.emotionId = emotionId;
     }
+
+
+    public void update3(Long date, String content, Integer emotionId) {
+        this.date = date;
+        this.content = content;
+        this.emotionId = emotionId;
+    }
 }
