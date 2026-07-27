7e07f6c567dbfed8aa87e4f52be0b44173fadd47
전민욱 <mwzzang00@gmail.com>
2026-07-27 11:19:31 +0900

    feat/first_branch

--- src/main/java/com/example/shinhangaecheokja/controller/UserController.java
+++ src/main/java/com/example/shinhangaecheokja/controller/UserController.java
@@ -10,17 +10,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
-@RequiredArgsConstructor
+@RequestMapping("/api/users")   //앞으로 회원과 관련된 모든 요청은 무조건 이쪽으로 찾아오세요!
+@RequiredArgsConstructor    //UserService같은 필수 직원들을 스프링이 알아서 데스크에 배치(의존성 주입)
 public class UserController {
 
-    private final UserService userService;
+    private final UserService userService;  //controller에서 직접하지 않고 userservice에게 넘김
 
     // 회원가입 API
-    @PostMapping("/signup")
-    public ResponseEntity<UserResponse> signUp(@Valid @RequestBody SignUpRequest request) {
+    @PostMapping("/signup") //데이터를 새로 생성하는 POST방식을 사용해,
+    public ResponseEntity<UserResponse> signUp(@Valid @RequestBody SignUpRequest request) { 
+        //ResponseEntity<UserResponse> = 손님에게 보낼 최종 결과물의 규격, UserResponse상자를 돌려주겠다
+        //@vaild = 1차 검사, 이메일등이 이상한 값이 들어오면 컷 해버리기
+        //@RequsetBody = JSON데이터를 자바가 읽을 수 있는 객체로 변환
         UserResponse response = userService.signUp(request);
+        //userService에서 작업을 한 후 UserResponse상자를 돌려줘
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        //프론트엔드에서 정확히 알 수 있도록 201 CREATED(새로운 데이터 생성!)라는 상태 코드 팻말을 세운뒤, response를 담아서 보냄.
     }
 }
 
