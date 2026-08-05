d08ae8ddca74b7fbafe9c0cbd147fc368ebeaa08
kms <kms75229911@gmail.com>
2026-08-05 11:44:13 +0900

    Revert "fix: V23 마이그레이션에 무중단 DDL 옵션(ALGORITHM=INPLACE, LOCK=NONE) 추가"
    This reverts commit 9c8d29095e5767b6ac6b7049813bbbf2a7b560a1.

--- src/main/resources/db/migration/V23__add_delivery_payment_idempotency_key.sql
+++ src/main/resources/db/migration/V23__add_delivery_payment_idempotency_key.sql
@@ -1,4 +1,4 @@
-ALTER TABLE delivery_request ADD COLUMN payment_idempotency_key VARCHAR(100) NULL, ALGORITHM=INPLACE, LOCK=NONE;
+ALTER TABLE delivery_request ADD COLUMN payment_idempotency_key VARCHAR(100) NULL;
 
 CREATE UNIQUE INDEX uq_delivery_request_customer_payment_key
-    ON delivery_request (customer_id, payment_idempotency_key), ALGORITHM=INPLACE, LOCK=NONE;
+    ON delivery_request (customer_id, payment_idempotency_key);
