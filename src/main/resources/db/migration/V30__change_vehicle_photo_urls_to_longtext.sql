d0f52f673c125897314a05e0320b69acd19aae0d
전민욱 <mwzzang00@gmail.com>
2026-08-12 15:45:02 +0900

    마이그레이션 선점 수정

--- src/main/resources/db/migration/V30__change_vehicle_photo_urls_to_longtext.sql
+++
@@ -1,4 +0,0 @@
--- linter:ignore-online-ddl
-ALTER TABLE vehicle MODIFY COLUMN insurance_photo_url LONGTEXT NULL;
--- linter:ignore-online-ddl
-ALTER TABLE vehicle MODIFY COLUMN photo_url LONGTEXT NULL;
