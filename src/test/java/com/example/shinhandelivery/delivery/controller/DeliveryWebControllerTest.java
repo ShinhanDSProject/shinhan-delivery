f87d3b8c662c6c83f2fa2cd10d977cb226df9eb4
jaeya1006-arch <jaeya1006@gmail.com>
2026-08-16 21:17:20 +0900

    마이페이지 일부 메뉴 화면에서 하단 내비게이션이 누락되는 오류 수정 (#323)

--- src/test/java/com/example/shinhandelivery/delivery/controller/DeliveryWebControllerTest.java
+++ src/test/java/com/example/shinhandelivery/delivery/controller/DeliveryWebControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.shinhandelivery.delivery.controller;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -24,5 +26,15 @@ class DeliveryWebControllerTest {
         .perform(get("/delivery-history"))
         .andExpect(status().isOk())
         .andExpect(view().name("delivery-history"));
+  }
+
+  @Test
+  @DisplayName("배송 취소 내역 화면에는 고객 하단 내비게이션을 표시한다")
+  void deliveryCancelListShowsCustomerBottomNavigation() throws Exception {
+    mockMvc
+        .perform(get("/delivery-cancel-list"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("delivery-cancel-list"))
+        .andExpect(content().string(containsString("class=\"customer-bottom-nav\"")));
   }
 }
